a9e971f13867921009ef0bf03c5073f00c2daa01
Hegel Trigo <hegeltrigo@gmail.com>
2012-02-10 19:27:51 +0100

    adding entire project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mydepot::Application.config.secret_token = 'ce5b37b03e8a408f2cb74b7b67c75cc0d80ba804e9fe96382f52642e663801a725f1a06fd192b43e9403418fcddd56cdbbcd64e8212d2f479858219fda7671bd'
